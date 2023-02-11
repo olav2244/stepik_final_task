e0953c02c44b00d719b1069e6be103bfefa783e8
olav2244 <olav2244@gmail.com>
2023-02-11 15:01:33 +0400

    add_required_files_from_module_4_block_2_step_9

---
+++ pages/login_page.py
@@ -0,0 +1,24 @@
+from .locators import LoginPageLocators
+from .base_page import BasePage
+from selenium.webdriver.common.by import By
+
+
+class LoginPage(BasePage):
+
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        # реализуйте проверку на корректный url адрес
+        check_login_url = str(self.browser.current_url)
+        assert 'login' in check_login_url, "The url in not for the login page"
+
+    def should_be_login_form(self):
+        # реализуйте проверку, что есть форма логина
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "There is no login form"
+
+    def should_be_register_form(self):
+        # реализуйте проверку, что есть форма регистрации на странице
+        assert self.is_element_present(*LoginPageLocators.REGISTRATION_FORM), "There is no registration form"
