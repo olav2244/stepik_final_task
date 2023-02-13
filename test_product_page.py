dc264cbf151a4f2fbf75ea543f0c5f09fbef3662
olav2244 <olav2244@gmail.com>
2023-02-13 19:43:26 +0400

    add_required_files_from_module_4_block_3_step_3

--- test_product_page.py
+++ test_product_page.py
@@ -4,13 +4,13 @@ from .pages.product_page import ProductPage
 
 
 def test_guest_can_add_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+    # link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+    link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019'
     page = ProductPage(browser,
                        link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
     page.open()               # открываем страницу
     page.add_to_cart()        # добавляем товар в корзину
     page.solve_quiz_and_get_code()
-    time.sleep(100)
     page.should_be_added_to_cart()
     page.should_be_message_in_cart_total()
     time.sleep(10)
