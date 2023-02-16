b8ae43035bbf23be81cf10f8c4bd0458735f95ec
olav2244 <olav2244@gmail.com>
2023-02-16 18:58:18 +0400

    add_required_files_from_module_4_block_3_step_6

--- pages/product_page.py
+++ pages/product_page.py
@@ -44,3 +44,11 @@ class ProductPage(BasePage):
         message_basket_total = self.browser.find_element(*ProductPageLocators.MESSAGE_BASKET_TOTAL).text
         product_price = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE).text
         assert product_price == message_basket_total, "No product price in the message"
+
+    def should_not_be_success_message(self):
+        assert self.is_not_element_present(*ProductPageLocators.MESSAGE_ABOUT_ADDING), \
+                "Success message is presented, but should not be"
+
+    def success_message_should_disappear(self):
+        assert self.is_disappeared(*ProductPageLocators.MESSAGE_ABOUT_ADDING), \
+            "Success message is presented, but should disappear"
--- test_product_page.py
+++ test_product_page.py
@@ -3,7 +3,7 @@ import pytest
 
 from .pages.product_page import ProductPage
 
-
+'''
 @pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
@@ -27,3 +27,29 @@ def test_guest_can_add_product_to_basket(browser, link):
     page.should_be_added_to_cart()
     page.should_be_message_in_cart_total()
     time.sleep(2)
+'''
+
+
+@pytest.mark.xfail(reason="Failing expected")
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+    link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_to_cart()
+    page.should_not_be_success_message()
+
+
+def test_guest_cant_see_success_message(browser):
+    link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_not_be_success_message()
+
+
+@pytest.mark.xfail(reason="Failing expected")
+def test_message_disappeared_after_adding_product_to_basket(browser):
+    link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_to_cart()
+    page.success_message_should_disappear()
